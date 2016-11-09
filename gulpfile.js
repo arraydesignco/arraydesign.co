dbbc28aa30d3d6ddc21dccc18add030308a48b54
Benjamin Inada <benjamin.inada@gmail.com>
2016-11-09 03:35:02 -0800

    Add fancy schmancy button

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gulp = require('gulp');
 var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
@@ -13,7 +15,7 @@ gulp.task('minify-css', function() {
   return gulp.src('css/creative.css')
     .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('css'));
 });
 
 // Minify JS
@@ -22,7 +24,7 @@ gulp.task('minify-js', function() {
     .pipe(uglify())
     .pipe(header(banner, { pkg: pkg }))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('js'))
+    .pipe(gulp.dest('js'));
 });
 
 gulp.task('sass', function () {
@@ -40,19 +42,19 @@ gulp.task('jshint', function() {
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
   gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-    .pipe(gulp.dest('vendor/bootstrap'))
+    .pipe(gulp.dest('vendor/bootstrap'));
 
   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('vendor/jquery'))
+    .pipe(gulp.dest('vendor/jquery'));
 
   gulp.src(['node_modules/magnific-popup/dist/*'])
-    .pipe(gulp.dest('vendor/magnific-popup'))
+    .pipe(gulp.dest('vendor/magnific-popup'));
 
   gulp.src(['node_modules/scrollreveal/dist/*.js'])
-    .pipe(gulp.dest('vendor/scrollreveal'))
+    .pipe(gulp.dest('vendor/scrollreveal'));
 
   gulp.src(['bower_components/parallax.js/*.js'])
-    .pipe(gulp.dest('vendor/parallax.js'))
+    .pipe(gulp.dest('vendor/parallax.js'));
 
   gulp.src([
       'node_modules/font-awesome/**',
@@ -62,7 +64,10 @@ gulp.task('copy', function() {
       '!node_modules/font-awesome/*.md',
       '!node_modules/font-awesome/*.json'
   ])
-    .pipe(gulp.dest('vendor/font-awesome'))
+    .pipe(gulp.dest('vendor/font-awesome'));
+
+  gulp.src(['vendor/bourbon/**'])
+    .pipe(gulp.dest('sass/vendor/bourbon'));
 })
 
 gulp.task('connect', function() {
@@ -80,7 +85,7 @@ gulp.task('reload', function () {
 gulp.task('watch', function () {
   gulp.watch(['./*.html', './img/*', './fonts/*'], ['reload']);
   gulp.watch(['./js/**/*.js'], ['jshint', 'reload']);
-  gulp.watch(['./sass/**/*.scss'], ['sass', 'reload'])
+  gulp.watch(['./sass/**/*.scss'], ['sass', 'reload']);
 });
  
 // For development
