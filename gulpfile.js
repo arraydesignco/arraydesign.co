7053e3d6556db9327af8d9f4e7324586fe48f2f1
Benjamin Inada <benjamin.inada@gmail.com>
2016-11-20 14:41:02 -0800

    Organize asset files, move icons, use minified css and js.

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var pkg = require('./package.json');
 
 // Minify compiled CSS
 gulp.task('minify-css', function () {
-  return gulp.src('css/creative.css')
+  return gulp.src('css/main.css')
     .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('css'));
@@ -23,9 +23,8 @@ gulp.task('minify-css', function () {
 
 // Minify JS
 gulp.task('minify-js', function () {
-  return gulp.src('js/creative.js')
+  return gulp.src('js/main.js')
     .pipe(uglify())
-    .pipe(header(banner, { pkg: pkg }))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('js'));
 });
@@ -37,27 +36,27 @@ gulp.task('sass', function () {
 });
 
 gulp.task('jshint', function () {
-  return gulp.src('js/**/*.js')
+  return gulp.src(['js/**/*.js', '!js/**/*.min.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-// Copy vendor libraries from /node_modules into /vendor
+// Copy assets into the right areas
 gulp.task('copy', function () {
   gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-    .pipe(gulp.dest('vendor/bootstrap'));
+    .pipe(gulp.dest('assets/bootstrap'));
 
   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('vendor/jquery'));
+    .pipe(gulp.dest('assets/jquery'));
 
   gulp.src(['node_modules/magnific-popup/dist/*'])
-    .pipe(gulp.dest('vendor/magnific-popup'));
+    .pipe(gulp.dest('assets/magnific-popup'));
 
   gulp.src(['node_modules/scrollreveal/dist/*.js'])
-    .pipe(gulp.dest('vendor/scrollreveal'));
+    .pipe(gulp.dest('assets/scrollreveal'));
 
   gulp.src(['bower_components/parallax.js/*.js'])
-    .pipe(gulp.dest('vendor/parallax.js'));
+    .pipe(gulp.dest('assets/parallax.js'));
 
   gulp.src([
       'node_modules/font-awesome/**',
@@ -67,10 +66,10 @@ gulp.task('copy', function () {
       '!node_modules/font-awesome/*.md',
       '!node_modules/font-awesome/*.json'
   ])
-    .pipe(gulp.dest('vendor/font-awesome'));
+    .pipe(gulp.dest('assets/font-awesome'));
 
-  gulp.src(['vendor/bourbon/**'])
-    .pipe(gulp.dest('sass/vendor/bourbon'));
+  gulp.src(['assets/bourbon/**'])
+    .pipe(gulp.dest('sass/assets/bourbon'));
 })
 
 gulp.task('connect', function () {
@@ -97,10 +96,5 @@ gulp.task('watch', function () {
 
 // For development
 gulp.task('default', function () {
-  runSequence('copy', ['jshint', 'sass'], 'connect', 'watch');
-});
-
-// When ready to deploy
-gulp.task('package', function () {
-  runSequence('copy', ['jshint', 'sass'], ['minify-css', 'minify-js']);
+  runSequence('copy', ['jshint', 'sass'], ['minify-css', 'minify-js'], 'connect', 'watch');
 });
