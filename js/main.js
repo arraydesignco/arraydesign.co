dd40f1d5eed91b29621d966034d9a706782b87ad
Ben Inada <me@beninada.com>
2016-11-21 01:06:44 -0800

    Add primitive carousel.

--- js/main.js
+++ js/main.js
@@ -66,6 +66,32 @@
     $('#hook').parallax({imageSrc: 'img/growth.jpg'});
     $('#contact').parallax({imageSrc: 'img/phone.jpg'});
 
+    // Initialize carousel
+    var $item = $('.carousel .item'); 
+    var $wHeight = $(window).height() * .75;
+
+    $item.eq(0).addClass('active');
+    $item.height($wHeight); 
+    $item.addClass('full-screen');
+
+    $('.carousel img').each(function() {
+      var $src = $(this).attr('src');
+      var $color = $(this).attr('data-color');
+      $(this).parent().css({
+        'background-image' : 'url(' + $src + ')',
+        'background-color' : $color
+      });
+      $(this).remove();
+    });
+
+    $(window).on('resize', function (){
+      $wHeight = $(window).height() *.75;
+      $item.height($wHeight);
+    });
+
+    $('.carousel').carousel();
+
+    // Initialize scrolldown
     $(function() {
         $('.scroll-down').click(function() {
             $('html, body').animate({scrollTop: $('#intro').offset().top }, 'slow');
